1c6d7b75e8ffad25efdd35f278eac07ea7d53ada
oliviam20 <oliviatrademark@gmail.com>
2021-10-13 19:31:59 +1100

    add hero stripes component, already imported into app

---
+++ src/components/HeroStripes/HeroStripes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HeroStripes.scss";
+
+const HeroStripes = () => (
+  <div id="stripes">
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+  </div>
+);
+
+export default HeroStripes;
